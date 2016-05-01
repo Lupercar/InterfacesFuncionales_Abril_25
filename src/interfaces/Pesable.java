a0cb3fa2acd9cecb4e2210942eacf766af1e8854
JLGG <hotolete@gmail.com>
2016-05-02 00:54:28 +0200

    Creo interface funcional Pesable.

---
+++ src/interfaces/Pesable.java
@@ -0,0 +1,9 @@
+package interfaces;
+/*
+ * Las interfaces funcionales son interfaces pero en JAVA8 se le llaman 
+ * así a las interfaces que tienen un unico metodo abstracto al que 
+ * poner codigo. 
+ */
+public interface Pesable {
+	double getPeso(); //metodo que nos devuelva el peso.
+}
