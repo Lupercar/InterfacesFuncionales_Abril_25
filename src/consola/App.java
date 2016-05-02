a1746c2d747d4d4879801ee30e27251a452be245
JLGG <hotolete@gmail.com>
2016-05-02 02:06:07 +0200

    A partir de Java8 empleamos las expresiones lambda para las interfaces funcionales

--- src/consola/App.java
+++ src/consola/App.java
@@ -7,7 +7,7 @@ public class App {
 	public static void main(String[] args) {
 //		creo un objeto de esa interface Pesable, 
 //		objeto que debe implementar el metodo que tiene la interfaz
-		Pesable p = new Pesable() {
+		Pesable p1 = new Pesable() {
 			
 			@Override
 			public double getPeso() {
@@ -15,5 +15,13 @@ public class App {
 				return 0;
 			}
 		};
+		
+//		A partir de Java8 tenemos las expresiones lambda, la cual 
+//		nos devuelve un codigo fuente. 
+		Pesable p2 = () -> {//varias líneas de codigo con llaves + comando return si hace falta.
+								return 0.0; 
+							};
+		
+		Pesable p3 = () -> 0.0; //una linea de codigo se entiende que es el return
 	}
 }
